2087e2daa21179c6ad5345f7f1a05a8c6d826855
Dennis Costa <dennismeira.costa@gmail.com>
2023-09-25 17:13:00 -0300

    theme defined, working on global style

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={ theme }>
+      <GlobalStyles />
+      
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+    COLORS: {
+        PINK: "#FF859B",
+        WHITE: "#F4EDE8",
+
+        BACKGROUND_600: "#312E38",
+        BACKGROUND_700: "#262529",
+        BACKGROUND_800: "#1C1B1E",
+        BACKGROUND_900: "#0D0C0F",
+
+        GRAY_100: "#E5E5E5"
+        GRAY_200: "#999591"
+        GRAY_300: "#948F99"
+    } 
+}
